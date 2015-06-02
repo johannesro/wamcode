e46b709d3295ce33ef8ce56e67bd8ba880dfdc91
Ana Carrasco <anac@pc2865.pc.met.no>
2015-06-02 15:07:25 +0200

    add wam_netcdf_metno_module to the compiling script

--- WAM/mk/makefile_ubuntu.mk
+++ WAM/mk/makefile_ubuntu.mk
@@ -47,7 +47,7 @@ pnetcdf: wam_mpi_module.o wam_file_module.o wam_timopt_module.o \
 	wam_model_module.o wam_source_module.o wam_fre_dir_module.o wam_interface_module.o \
 	wam_grid_module.o wam_current_module.o wam_special_module.o wam_nest_module.o \
 	wam_ice_module.o wam_output_module.o wam_print_module.o wam_general_module.o \
-	wam_output_set_up_module.o wam_mpi_comp_module.o wam_netcdf_module.o wam_coordinate_module.o \
+	wam_output_set_up_module.o wam_mpi_comp_module.o wam_netcdf_metno_module.o wam_coordinate_module.o \
 	read_current_input.o read_ice_input_metno.o wam_topo_module.o read_topo_input.o jafu.o \
 	make_netcdf.o 
 	$(FC) $(INC) $(FFLAGS) $^ -lmpi -L${NCDIR}/lib -lnetcdff -o $@ 
@@ -97,7 +97,7 @@ read_wind_input_metno.o: read_wind_input_metno.f90
 read_ice_input_metno.o: read_ice_input_metno.f90
 	$(FC) $(INC) $(FFLAGS) -I${NCDIR}/include -L${NCDIR}/lib -lnetcdff  -c -o $@ $<
 
-wam_netcdf_module.o: wam_netcdf_module.f90
+wam_netcdf_metno_module.o: wam_netcdf_metno_module.f90
 	$(FC) $(INC) $(FFLAGS) -I${NCDIR}/include -L${NCDIR}/lib -lnetcdff  -c -o $@ $<
 
 clean:
